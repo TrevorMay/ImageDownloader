7e110ca14ee724db58ba54e883a6776f84cf8459
Trevor May <trevormay2017@gmail.com>
2024-04-21 11:36:31 -0600

    removing images

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -2,7 +2,6 @@ import random
 import numpy as np
 import string
 import requests
-import shutil
 import os
 from bs4 import BeautifulSoup
 from PIL import Image
@@ -11,12 +10,12 @@ from io import BytesIO
 images_desired = 100
 # random_strings = set()
 base_url = "https://prnt.sc/"
-save_path = r"C:\repo\ImageDownloader\ImageDownloader\PrntScnImages\\"
-removed_image_path = r"C:\repo\ImageDownloader\ImageDownloader\PrntScnImages\assets\screenshot_removed.png"
-imgur_not_available = r"C:\repo\ImageDownloader\ImageDownloader\PrntScnImages\assets\imgur_not_available.png"
+save_path = r"C:\repo\ImageDownloader\PrntScnImages\\"
+removed_image_path = r"C:\repo\ImageDownloader\PrntScnImages\assets\screenshot_removed.png"
+imgur_not_available = r"C:\repo\ImageDownloader\PrntScnImages\assets\imgur_not_available.png"
 existing_screenshots = os.listdir(save_path)
 existing_screenshots = [screenshot[0:-4] for screenshot in existing_screenshots]
-tried_strings_path = r"C:\repo\ImageDownloader\ImageDownloader\PrntScnImages\assets\random_strings.txt"
+tried_strings_path = r"C:\repo\ImageDownloader\PrntScnImages\assets\random_strings.txt"
 
 
 def is_removed_image(downloaded_image_response):
@@ -24,12 +23,22 @@ def is_removed_image(downloaded_image_response):
     imgur_removed_image = Image.open(imgur_not_available)
     downloaded_image = Image.open(BytesIO(downloaded_image_response.content))
 
+    width, height = downloaded_image.size
+
     # Convert images to arrays and compare
     arr1 = np.array(prnt_sc_removed_image)
     arr2 = np.array(imgur_removed_image)
     arr3 = np.array(downloaded_image)
     is_imgur_removed = np.array_equal(arr3, arr2)
     is_prntsc_removed = np.array_equal(arr3, arr1)
+
+    """
+    looking for an exact image size is unfortunately one  of
+    the only reliable ways to find these.
+    """
+    if width == 422 and height == 108:
+        is_prntsc_removed = True
+
     return is_prntsc_removed or is_imgur_removed
 
 
@@ -42,24 +51,10 @@ with open(tried_strings_path, 'r') as file:
         # Remove the newline character at the end of each line and add the line to the list
         tried_strings.append(line.strip())
 
-# i = 0
-# while i < 1000:  # make us 1000 new strings
-#     rand_text = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-#     if rand_text not in existing_screenshots and rand_text not in random_strings and rand_text not in tried_strings:
-#         random_strings.add(rand_text)
-#         i += 1
-
-# print(random_strings)
-
-# with open(tried_strings_path, 'a') as file:
-#     # Iterate through each element in the list
-#     for element in random_strings:
-#         # Write the element to the file followed by a newline character
-#         file.write(element + '\n')
 i = 0
 while i < images_desired:
     rand_text = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-    while rand_text in existing_screenshots and rand_text not in random_strings and rand_text not in tried_strings:
+    while rand_text in existing_screenshots and rand_text not in tried_strings:
         rand_text = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
 
     image_url = base_url + rand_text
